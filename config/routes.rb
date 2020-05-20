194d29bf6740d2c81fe287e262e9c9996798a391
Arthur Song <arth3rs0ng@gmail.com>
2020-05-20 09:10:36 -0700

    Added route  to post authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :rooms
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'authenticate', to: 'authentication#authenticate'
 end
