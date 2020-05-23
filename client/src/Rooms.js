98c6aa3cff84be3966360e1bc72d15077b983e6c
Arthur Song <arth3rs0ng@gmail.com>
2020-05-23 12:49:24 -0700

    Added Log Out button, need to add functionality still

--- client/src/Rooms.js
+++ client/src/Rooms.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
 class Rooms extends React.Component {
+    logOut(){
+        this.props.logOut()
+    }
+
     render () {
         return (
             <div>
                 Rooms
+                <button onClick={this.logOut}>Log Out</button>
             </div>
         )
     }
