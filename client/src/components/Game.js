9b34fc23a7770f47a7bbde128c9c244ae2a477ee
Arthur Song <arth3rs0ng@gmail.com>
2020-06-13 17:49:08 -0700

    Users in a room, can now create and start a game Clicking start game will create new Game associated with room Also, game will automatically commence JSON rendered will include active_round

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -32,12 +32,28 @@ class Game extends React.Component {
         // POST /rounds/:id/games? Nested Route
         // set this.state.game with JSON
         // subscribe to game_channel
-        
+        const token = localStorage.getItem("token");
+        if (token) {
+            fetch(`http://localhost:3001/rooms/${this.props.room.id}/games`, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                }
+            }).then(resp => resp.json())
+                .then(json => {
+                    console.log(json);
+                    this.setState({
+                        game: json
+                    })
+                })
+        }
     }
 
     renderButton = () => {
         // if game is loaded don't render button
-        if (!this.state.game) {
+        if (this.state.game === undefined) {
             return <button onClick={this.createAndStartGame}>Start Game</button>
         }
     }
