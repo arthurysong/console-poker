25f13729dc52d9cb26741071a4f5786bd067850e
Arthur Song <arth3rs0ng@gmail.com>
2020-06-19 12:16:26 -0700

    Terminal now displays proper greetings message

--- client/src/components/GameConsole.js
+++ client/src/components/GameConsole.js
@@ -14,33 +14,36 @@ class GameConsole extends React.Component {
         const div = findDOMNode(this.refs.jterminal);
         const userId = this.props.user.id
         terminal(window, $);
-        this.term = $(div).terminal([{
-            fold: function(){
-                fetchWithToken(`http://localhost:3001/users/make_move`);
-                // this equals terminal
+        this.term = $(div).terminal([
+            {
+                fold: function(){
+                    console.log('fold');
+                    // fetchWithToken(`http://localhost:3001/users/make_move`);
+                    // this equals terminal
+                },
+                check: function() {
+                    console.log('check');
+                    // this.props.subscription.make_move('check')
+                },
+                call: function() {
+                    console.log('call')
+                    // this.props.subscription.make_move('call')
+                },
+                raise: function(x) {
+                    console.log('raise ', x)
+                    // this.props.subscription.make_move('raise')
+                },
+                allin: function() {
+                    console.log('allin')
+                    // this.props.subscription.make_move('allin')
+                }
+    
             },
-            check: function() {
-                console.log('check');
-                // this.props.subscription.make_move('check')
-            },
-            call: function() {
-                console.log('call')
-                // this.props.subscription.make_move('call')
-            },
-            raise: function(x) {
-                console.log('raise ', x)
-                // this.props.subscription.make_move('raise')
-            },
-            allin: function() {
-                console.log('allin')
-                // this.props.subscription.make_move('allin')
-            },
-
-        },(cmd, t) => {
+        (cmd, t) => {
             t.echo('user said ' + cmd);
-        }, {
+        }], {
             greetings: 'Game Terminal:'
-        }])
+        })
         this.props.round.status.forEach(s => this.term.echo(s))
     }
 
