62ff6e806b2b7f4d65368245564144f83594cb4d
Arthur Song <arth3rs0ng@gmail.com>
2020-06-27 16:47:07 -0700

    Added some styling to the Chatbox Component User messages appear in bubbles When user messages repeatedly the messages are grouped in same bubble

--- client/src/components/Chatbox.js
+++ client/src/components/Chatbox.js
@@ -19,26 +19,54 @@ class Chatbox extends React.Component {
         })
     }
 
+    createNewArray = () => {
+        let i = 0
+        let j = 0
+        let bubble = {}
+        let temp = []
+        while (i < this.props.messages.length) {
+            if (this.props.messages[j+1] === undefined || this.props.messages[j].username !== this.props.messages[j+1].username) {
+                bubble["username"] = this.props.messages[j].username
+                bubble["messages"] = this.props.messages.slice(i, j+1)
+                temp.push(bubble)
+                bubble = {}
+                i = ++j
+            } else {
+                // if j+1 is equal to j.. the we go to next j
+                j++
+            }
+        }
+        return temp
+    }
+
     renderMessages = () => {
         if (this.props.messages !== undefined) {
-            return (this.props.messages.map((message, index) => <li key={index}>{message.username}: {message.payload}</li>))
+            return (
+                this.createNewArray().map((bubble, index) => 
+                    <section key={index} className="message -left">
+                        {bubble.username}&nbsp;
+                        <div className="nes-balloon from-left">
+                            {bubble.messages.map((message, index2) => <div key={index2}>{message.payload}<br/></div>)}
+                        </div>
+                    </section>
+            ))
         } 
     }
 
     render() {
         return (
-            // <div>
-            <div className="nes-container">
-                Hello???
-                <p>
-                <ul>
-                    {this.renderMessages()}
-                </ul>
-                <form onSubmit={this.submitHandler}>
-                    <input type="textarea" onChange={this.changeHandler} value={this.state.newMessage}/>
+            <div id="chatbox_container" className="nes-container">
+                <div id="messages_container" className="nes-container">
+
+                    <section className="message-list">
+                        {this.renderMessages()}
+                    </section>
+                    
+                </div>
+                <form id="new_message_form" onSubmit={this.submitHandler}>
+                    <input type="textarea" id="textarea_field" className="nes-textarea" onChange={this.changeHandler} value={this.state.newMessage}/>
                     <input type="submit" value="send"/>
                 </form>
-                </p>
             </div>
         )
     }
