973f4763131286d54b17b236c9067139562f2105
Arthur Song <arth3rs0ng@gmail.com>
2020-06-28 15:08:29 -0700

    Cards are styled with color depending on the suit

--- client/src/components/GameBoard.js
+++ client/src/components/GameBoard.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { hashStringToColor } from '../utilities/colorHash'
+import { colorCard } from '../utilities/colorCards'
 
 class GameBoard extends React.Component {
+    styleCards = (cards) => {
+        const arr = cards.split(" ");
+        console.log(arr);
+        return (
+            <>
+            {arr.map((c,index) => (<span key={index} style={{color: colorCard(c)}}>{c}&nbsp;</span>))}
+            </>
+        )
+    }
+
     renderPlayerCards = user => {
         if (user.playing === true) {
             if (user.username === this.props.user.username) {
-                return (user.cards)
+                return (this.styleCards(user.cards))
             } else {
                 return "Xx Xx"
             }
@@ -25,7 +36,12 @@ class GameBoard extends React.Component {
         return (
             <>
                 {this.props.round.ordered_users.map((user,index) => 
-                    <li className="board_user" key={index}><span style={{color: `${hashStringToColor(user.username)}`}}>{user.username}</span><span className="board_user_chips">({user.chips})</span> {this.renderPlayerCards(user)} {this.renderDealerButton(user)}</li>)}
+                    <li className="board_user" key={index}>
+                        <span style={{color: `${hashStringToColor(user.username)}`}}>{user.username}
+                        </span>
+                        <span className="board_user_chips">({user.chips})
+                        </span>&nbsp;
+                        {this.renderPlayerCards(user)}{this.renderDealerButton(user)}</li>)}
             </>
         )
     }
