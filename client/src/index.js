768b08bf177d9ebc217d4b8a2f6c7adc37db1995
Arthur Song <arth3rs0ng@gmail.com>
2020-06-30 15:55:20 -0700

    Added Elements provider to index file Created stripePromise in index and provides to App components

--- client/src/index.js
+++ client/src/index.js
@@ -8,14 +8,20 @@ import thunk from 'redux-thunk';
 import reducer from './redux/reducer';
 import './fonts/stylesheet.css';
 import cableMiddleware from './redux/cableMiddleware';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 
 // const store = createStore(reducer, applyMiddleware(thunk, cableMiddleware))
 const store = createStore(reducer, applyMiddleware(cableMiddleware(), thunk))
+const stripePromise = loadStripe("pk_test_51GqNn2Kj8jVe4aIu3gu8fP0a3Y8ltGdN66FAP1vq1K2jt3PUvI9STlO8JrGOXAr3DQBx1FRNCZ7Ye5KByzTu7Hmx00LAxaeYGP");
+
 window.store = store;
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Elements stripe={stripePromise}>
+      <App />
+    </Elements>
   </Provider>,
   document.getElementById('root')
 );
