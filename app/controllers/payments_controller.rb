c07825cf63bfdf733ecd141119c020b177112398
Arthur Song <arth3rs0ng@gmail.com>
2020-06-30 15:50:11 -0700

    Generated payments controller to return client secret

---
+++ app/controllers/payments_controller.rb
@@ -0,0 +1,19 @@
+class PaymentsController < ApplicationController
+    Stripe.api_key = "sk_test_51GqNn2Kj8jVe4aIuNY5sxkfGCrpv5HAPSmMQdzkpJkvnTNYk2LCMQ0TD9jRpG9G8HmwmrUZRiizGcc2sFHaxgeEo00RsFY5nMT"
+    
+    def secret
+        intent = Stripe::PaymentIntent.create({
+            amount: 1099,
+            currency: 'usd',
+            # Verify your integration in this guide by including this parameter
+            metadata: {integration_check: 'accept_a_payment'},
+          })
+        render json: { client_secret: intent.client_secret }
+    end
+
+    private
+    
+    def payment_params
+
+    end
+end
