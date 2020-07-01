5ae7551718ae477b5b65612a4ef9976e4bf07b1f
Arthur Song <arth3rs0ng@gmail.com>
2020-07-01 11:52:03 -0700

    /users/:id/deposit will redirect if user is not logged in

--- client/src/redux/handleAuthRedirect.js
+++ client/src/redux/handleAuthRedirect.js
@@ -2,7 +2,7 @@ export default function handleAuthRedirect(isLoggedIn, history){
     let curr_url = window.location.pathname;
     if (isLoggedIn && (curr_url === "/" || curr_url === "/login" || curr_url === "/register")) { // redirect for setLogin if user is logged in
         history.push(`/rooms`);
-    } else if (!isLoggedIn && (curr_url === "/rooms" || curr_url === "/" || /^\/{1}rooms\/{1}.+$/.test(curr_url))) { // redirect for setLogin at rooms url if usuer is not logged in
+    } else if (!isLoggedIn && (curr_url === "/rooms" || curr_url === "/" || /^\/{1}rooms\/{1}.+$/.test(curr_url) || /^\/{1}users\/{1}.+$/.test(curr_url))) { // redirect for setLogin at rooms url if usuer is not logged in
         history.push(`/login`)
     } 
 }
