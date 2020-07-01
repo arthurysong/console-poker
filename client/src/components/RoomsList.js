41e28a652fa670bce49679f24c4ec5525b4e8d9d
Arthur Song <arth3rs0ng@gmail.com>
2020-07-01 15:46:33 -0700

    Styling buttons in RoomsList Component

--- client/src/components/RoomsList.js
+++ client/src/components/RoomsList.js
@@ -73,10 +73,10 @@ class RoomsList extends React.Component {
         return (
             <div>
                 {this.renderUser()}&nbsp;
-                <button className="nes-btn" onClick={this.clickHandler}>Log Out</button>&nbsp;
-                <button className="nes-btn is-success" onClick={this.redirectToDeposits}>Deposit</button><br/><br/>
+                <button className="nes-btn smaller-btn" id="test" onClick={this.clickHandler}>Log Out</button>&nbsp;
+                <button className="nes-btn is-success smaller-btn" onClick={this.redirectToDeposits}>Deposit</button><br/><br/><br/><br/>
                 <div className="ne"></div>
-                <NavLink to="/rooms/new" className="nes-btn is-primary">Create Room</NavLink><br/><br/>
+                <NavLink to="/rooms/new" className="nes-btn is-primary smaller-btn">Create Room</NavLink><br/><br/>
                 <ul>
                     {this.renderRooms()}
                 </ul>
