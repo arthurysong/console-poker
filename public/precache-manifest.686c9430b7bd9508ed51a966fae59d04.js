e73f0f25825eabdf9e274a80fcf9ad75536af574
Arthur Song <arth3rs0ng@gmail.com>
2020-07-04 17:23:26 -0700

    Ready for first push to heroku

---
+++ public/precache-manifest.686c9430b7bd9508ed51a966fae59d04.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1dac085b01d694b133c24fa73f81c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1220b4efbaba35f1bdef",
+    "url": "/static/css/2.be79674a.chunk.css"
+  },
+  {
+    "revision": "8d7317632b807cdc3325",
+    "url": "/static/css/main.6249bfcc.chunk.css"
+  },
+  {
+    "revision": "1220b4efbaba35f1bdef",
+    "url": "/static/js/2.4b4f984f.chunk.js"
+  },
+  {
+    "revision": "c6591730863aafdcd1e29a218fb67b3d",
+    "url": "/static/js/2.4b4f984f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d7317632b807cdc3325",
+    "url": "/static/js/main.da645f70.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4db14a6542c06043f8f3dbeb079de84a",
+    "url": "/static/media/atariclassic-regular-webfont.4db14a65.woff"
+  },
+  {
+    "revision": "6e2f7cd74c1e9f213f563065bc603aa0",
+    "url": "/static/media/atariclassic-regular-webfont.6e2f7cd7.woff2"
+  }
+]);
